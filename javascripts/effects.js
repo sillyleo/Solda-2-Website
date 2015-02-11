af8ed08e8706c8f73863cd3b72a43e7ac2ee8419
Leo <leolin@me.com>
2015-02-11 17:54:39 +0800

    faq 可以收起來

--- javascripts/effects.js
+++ javascripts/effects.js
@@ -3,4 +3,10 @@ $(document).ready(function() {
 	$.typer.options.highlightSpeed = 100;
 	$.typer.options.typeSpeed = 100;
 	$.typer.options.clearDelay = 1500;
+
+$('#support li').click(function(){
+	$(this).toggleClass('show-answer');
+});
+// $('#questions li').toggleClass('show-answer');
+
 });
--- javascripts/min/effects-min.js
+++ javascripts/min/effects-min.js
@@ -1,1 +1,1 @@
-$(document).ready(function(){$("[data-typer-targets]").typer(),$.typer.options.highlightSpeed=100,$.typer.options.typeSpeed=100,$.typer.options.clearDelay=1500});
+$(document).ready(function(){$("[data-typer-targets]").typer(),$.typer.options.highlightSpeed=100,$.typer.options.typeSpeed=100,$.typer.options.clearDelay=1500,$("#support li").click(function(){$(this).toggleClass("show-answer")})});
